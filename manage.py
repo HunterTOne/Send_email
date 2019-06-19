3ceb3c648d632574fe19e100f9827ea329c9f4c7
shallow <728363298@qq.com>
2019-06-19 11:23:27 +0800

    修改manage.py

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@
 from sms import create_app, db
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-
+from flask import request
 
 # 创建flask的应用对象
 app = create_app("develop")
